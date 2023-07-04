ea5588f903007ebe1b04ae2fcd125066d0352890
Oscar Perez-Hernandez <perezoscar360@gmail.com>
2023-07-03 22:19:50 -0500

    added all the server files now need to put in the code in some of the files in order to get my server running

---
+++ alex-garage-door-installation/server/server.js
@@ -0,0 +1,31 @@
+
+const express = require("express");
+const cors = require("cors")
+const db = require("./config/configDB")
+
+const dotenv = require("dotenv")
+dotenv.config()
+const routes = require("./routes")
+const PORT = process.env.PORT
+
+
+const app = express()
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Your Server is up and running")
+})
+
+app.use(routes)
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', () => {
+    app.listen(PORT, (err) => {
+        if (err) console.log(err)
+        console.log(`Server running on http://localhost:${PORT}`);
+
+    })
+})
